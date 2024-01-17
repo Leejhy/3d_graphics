bddf1c05e481fa7ce84801113b289862ae352a9f
Leejhy <jeehyeong0128@gmail.com>
2024-01-17 16:45:01 +0900

    22

--- fdf.c
+++ fdf.c
@@ -12,12 +12,23 @@
 
 #include "fdf.h"
 
+typedef struct 	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "hello world!");
-	mlx_loop(mlx);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	// mlx_loop(mlx);
 }
